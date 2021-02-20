c9db7dcb2d8e724b3b6d944f7149f4728b0dc63e
Mike7860 <michal.78.60@gmail.com>
2021-02-20 13:01:56 +0100

    Added click functionalities

--- DNA_check.py
+++ DNA_check.py
@@ -1,13 +1,52 @@
 import os
+import click
 import csv
-import textwrap
 import logging
 from datetime import date
+
+
+@click.command()
+# @click.option('--h', help='Simple program to identifying common mutations and analyzing genomes for the existence of specific substrings')
+@click.option('--input_csv', prompt='Path to input CSV', help='Path to input CSV')
+@click.option('--out_vis', prompt='Path to output pdf', help='Path to output pdf')
+@click.option('--out_csv', prompt='Path to output CSV', help='Path to output CSV')
+@click.option('--searched_domain', prompt='String to be searched', help='String to be searched')
+@click.option('--reference_id', prompt='ID of the reference genome', help='ID of the reference genome')
+def datas_check(input_csv, out_vis, out_csv, searched_domain, reference_id):
+    """Simple program to identifying
+    common mutations and analyzing genomes for the existence of specific
+    substrings."""
+    if input_csv:
+        pass
+    else:
+        raise Exception("Lack of file name")
+
+    if out_vis:
+        pass
+    else:
+        raise Exception("Lack of file name")
+
+    if out_csv:
+        pass
+    else:
+        raise Exception("Lack of file name")
+
+    if searched_domain:
+        pass
+    else:
+        raise Exception("Lack of searched domain")
+
+    if reference_id:
+        pass
+    else:
+        raise Exception("Lack of reference id")
+
 
 searched_domain = ""
 reference_id = ""
 dict_input_csv_all = {}
 a = 0
+date = date.today()
 
 
 class Genom:
@@ -19,21 +58,35 @@ class Genom:
         self.isDomainPresent = isDomainPresent
 
 
-with open('genomes.csv', newline='') as input_csv:
-    all = csv.reader(input_csv)
-    for row in all:
-        if row[0] == "id" and row[1] == "genome":
-            print("ok")
-        if row[0] == "M_AURIS_702100":
-            reference_id = row[1]
-            #print(reference_id)
-        if row[1] != reference_id:
-            a += 1
-            print(a)
-        dict_input_csv_all[row[0]] = Genom(row[0], row[0], row[0], row[0], row[0])
+#def open_input(input_csv):
+    print(input_csv)
+    # with open(input_csv, newline='') as input_csv:
+    #     all = csv.reader(input_csv)
+    #     print("Type searched part of DNA: ")
+    #     searched_domain = input()
+    #     for row in all:
+    #         if row[0] == "id" and row[1] == "genome":
+    #             print("ok")
+    #     if row[0] == "M_AURIS_702100":
+    #         reference_id = row[1]
+    #         # print(reference_id)
+    #     if row[1] != reference_id:
+    #         # print(a)
+    #         pass
+    #     dict_input_csv_all[row[0]] = Genom(row[0], row[0], row[0], row[0], row[0])
+    #
+    #     if searched_domain in row[1]:
+    #         # a += 1
+    #         # print(a)
+    #         pass
+    #
+    #     if not "A" and "C" and "T" and "G" and "N" in row[1]:
+    #         a += 1
+    #         print(a)
+    #
+    # # for i in sorted(dict_input_csv_all):
+    # #     print(i)
 
-    # for i in sorted(dict_input_csv_all):
-    #     print(i)
 
 with open('final.csv', 'w') as out_csv:
     fieldnames = ["id", "genome", "count", "mutation", "isDomainPresent"]
@@ -49,7 +102,11 @@ with open('final.csv', 'w') as out_csv:
             "isDomainPresent": value.isDomainPresent
         })
 
+    #
     # for key, value in dict_input_csv_all.items():
-    #     Test_row = "key: %s | value: %s | %s | %s | %s" % (key, value.genome, value.id, value.id, value.id)
+    #     genome_row = "key: %s | value: %s | %s | %s | %s" % (key, value.id, value.genome, value.count, value.mutation, value.isDomainPresent)
     #     logging.basicConfig(filename=str(date) + " logs.log", filemode='w', level=logging.INFO)
-    #     logging.info(Test_row)
+    #     logging.info(genome_row)
+
+if __name__ == '__main__':
+    datas_check()
